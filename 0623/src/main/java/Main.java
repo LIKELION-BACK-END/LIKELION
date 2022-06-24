531500cafa460cf33957e4a26658b2450381c91d
zzerowin <alaykr123@gmail.com>
2022-06-24 23:15:45 +0900

    🦁LIKELION 0624

--- 0623/src/main/java/Main.java
+++ 0623/src/main/java/Main.java
@@ -1,13 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-
         Bicycle myBicycle = new Bicycle("red", 100);
         myBicycle.info();
 
         myBicycle.color = "green";
         myBicycle.info();
-
     }
 }
 
