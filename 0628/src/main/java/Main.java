a39c6ca5436c4b61354ed6c9e0b5001e58009c90
zzerowin <alaykr123@gmail.com>
2022-06-28 23:06:14 +0900

    상속 특징

--- 0628/src/main/java/Main.java
+++ 0628/src/main/java/Main.java
@@ -2,6 +2,24 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Outer oc = new Outer();
+        System.out.println("oc.num : " + oc.num);
+        System.out.println("oc.str1 : " + oc.str1);
 
+        System.out.println();
+
+        // 맴버 inner 클래스
+        Outer.InnerClass in = oc.new InnerClass();
+        System.out.println("in.num : " + in.num);
+        System.out.println("in.str2 : " + in.str2);
+
+        System.out.println();
+
+        // static inner 클래스
+        Outer.SInnerClass si = new Outer.SInnerClass();
+        System.out.println("si.num : " + si.num);
+        System.out.println("si.str3 : " + si.str3);
+
+        System.out.println();
     }
 }
---
+++ 0628/src/main/java/Outer.java
@@ -0,0 +1,26 @@
+public class Outer {
+    int num = 10;
+    String str1 = "java";
+    static String str11 = "world";
+
+    public Outer() {
+        System.out.println("Outer 생성자");
+    }
+    class InnerClass {
+        int num = 100;
+        String str2 = str1;
+
+        public InnerClass() {
+            System.out.println("InnerClass 생성자");
+        }
+    }
+
+    static class SInnerClass {
+        int num = 1000;
+        String str3 = str11;
+
+        public SInnerClass() {
+            System.out.println("static InnerClass constructor");
+        }
+    }
+}
