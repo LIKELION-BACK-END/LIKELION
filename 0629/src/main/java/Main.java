c9f41e1097c1827bbd36359130f4b40c5c1143ba
zzerowin <alaykr123@gmail.com>
2022-06-29 11:51:15 +0900

    매개변수 사용

--- 0629/src/main/java/Main.java
+++ 0629/src/main/java/Main.java
@@ -1,34 +1,32 @@
-// 문제 : 레퍼런스변수에 값 할당과정 설명
-
 class Main {
     public static void main(String[] args) {
-        // 오직 Car 리모콘만 저장 할 수 있는 변수 `aCar`를 만든다.
-        // `aCar` 에는 Car 클래스로 만든 리모콘(쓰레기 리모콘)이 들어가게 된다.
-        Car aCar;
+        계산기.더한결과를_화면에_출력(10, 20);
+        // 출력 => 결과 : 30
 
-        // Car 설계도(클래스)로 Car 객체를 변수 외!부!에 만든다.
-        // Car 객체는 자신을 조종할 수 있는 리모콘(this)를 가지고 태어난다. 즉 Car 객체 만들 때 Car 리모콘도 같이 만들어진다.
-        // Car 객체는 자신이 들고 있는 리모콘(this)를 복사해서 반환한다.
-        // aCar 안에 들어있던 쓰레기 리모콘이 진짜 리모콘으로 대체된다.
-        aCar = new Car();
+        계산기.더한결과를_화면에_출력(50, 20);
+        // 출력 => 결과 : 70
 
-        // `aCar` 변수에 안에 있는 리모콘의 `달리다` 버튼을 누른다.
-        // 해당 신호가 리모콘과 연결되어 있는 Car 객체에 전달되어 `달리다` 기능이 실행된다.
-        aCar.run();
+        계산기.뺀결과를_화면에_출력(50, 20);
+        // 출력 => 결과 : 30
 
-        // 모든 객체들은 서로를 평생 볼 수 없다.
-        // 모든 객체들은 서로를 원격조종한다.
+        계산기.뺀결과를_화면에_출력(5, 2);
+        // 출력 => 결과 : 3
+
+        계산기.곱한결과를_화면에_출력(5, 2);
+        // 출력 => 결과 : 10
     }
 }
 
-// Car 설계도!, 절대 Car가 아니다.!!!
-class Car {
-    // 이 메서드(함수)의 2가지 용도
-    // - 실제 기능
-    // - 리모콘 버튼
-    int maxSpeed = 250;
-    String km = "km";
-    void run() {
-        System.out.println("자동차 최고속도 : " + maxSpeed + km);
+class 계산기 {
+    public static void 더한결과를_화면에_출력(int i, int x) {
+        System.out.println("i + x : " + (i + x));
+    }
+
+    public static void 뺀결과를_화면에_출력(int i, int x) {
+        System.out.println("i - x : " + (i - x));
+    }
+
+    public static void 곱한결과를_화면에_출력(int i, int x) {
+        System.out.println("i * x : " + (i * x));
     }
 }
