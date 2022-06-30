e1498ee6adc29df36a3799bb142bbcb7c6472199
zzerowin <alaykr123@gmail.com>
2022-06-30 15:14:28 +0900

    class와 extents 실행되는 세부과정

--- 0630/src/main/java/Main.java
+++ 0630/src/main/java/Main.java
@@ -1,117 +1,31 @@
-// 상황 : DVD방에 3개의 서로다른 tv가 있다. 영업을 하다보니 각 tv의 특별한 기능은 거의 사용될 일이 없고 기본적인 켜기/끄기 만 사용된다. 현재 리모콘이 3개나 있는습니다.
-// 문제 : 알바를 편하게 하기 위해서 표준Tv 리모콘을 구현해주세요.
+// 문제 : `a무기.공격();` 가 실행되는 세부과정을 자세히 쓰세요.
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("== 표준Tv 리모콘 들여오기 전 ==");
+        무기 a무기 = new 칼();
 
-        샤오미Tv a샤오미Tv = new 샤오미Tv();
-        삼성Tv a삼성Tv = new 삼성Tv();
-        LGTv aLGTv = new LGTv();
+        // `a무기` 변수(창고)에게 접근한다.
+        // 창고의 문을 연다.
+        // 무기가 있을 줄 알았는데 무기 리모콘이 있다.
+        // 무기리모콘의 버튼중에서 `공격` 버튼을 누르려고 시도한다.
+        // 버튼을 못찾아서 실패!!
 
-        a샤오미Tv.켜기();
-        // 출력 => 샤오미Tv 켜집니다.
-        a샤오미Tv.끄기();
-        // 출력 => 샤오미Tv 꺼집니다.
-        a샤오미Tv.vr켜기();
-        // 출력 => 샤오미Tv vr켜기!
+        // * 안되는 이유
+        // 실제로 리모콘에 연결된 칼 객체는 `공격` 기능을 가지고 있다.
+        // 안되는 이유는 단순히 버튼이 없어서 이다.
 
-        a삼성Tv.켜기();
-        // 출력 => 삼성Tv 켜집니다.
-        a삼성Tv.끄기();
-        // 출력 => 삼성Tv 꺼집니다.
-        a삼성Tv.ar켜기();
-        // 출력 => 삼성Tv ar켜기!
+        // a무기.공격();
 
-        aLGTv.켜기();
-        // 출력 => LGTv 켜집니다.
-        aLGTv.끄기();
-        // 출력 => LGTv 꺼집니다.
-        aLGTv.게임모드전환();
-        // 출력 => LGTv 게임모드전환!
-
-        System.out.println("== 표준Tv 리모콘 들여온 후 ==");
-
-        // 표준Tv 리모콘을 만든다.
-        표준Tv a표준Tv;
-
-        // a표준Tv 변수에 샤오미Tv 객체를 연결한다.
-        a표준Tv = a샤오미Tv;
-        a표준Tv.켜기();
-        // 출력 : 샤오기TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 샤오기TV 꺼집니다.
-
-        // a표준Tv 변수에 삼성Tv 객체를 연결한다.
-        a표준Tv = a삼성Tv;
-        a표준Tv.켜기();
-        // 출력 : 삼성TV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : 삼성TV 꺼집니다.
-
-        // a표준Tv 변수에 LGTv 객체를 연결한다.
-        a표준Tv = aLGTv;
-        a표준Tv.켜기();
-        // 출력 : LGTV 켜집니다.
-        a표준Tv.끄기();
-        // 출력 : LGTV 꺼집니다.
-
-        // LGTV만의 고유 기능을 표준Tv 리모콘을 이용해서 호출하기 => 불가능
-        // (LGTv) => a표준Tv 변수에 있던 표준Tv 리모콘이 LGTv리모콘화 해서 `aLGTv2` 변수에 들어간다.
-        LGTv aLGTv2 = (LGTv)a표준Tv;
-        aLGTv2.게임모드전환();
+        // 자바에서는 위험하다고 판단되면, 통과시켜주지 않는다 => 개발자의 서명 필요
+        // 자바에서는 리모콘의 버튼이 추가되는 것을 극도로 무서워한다. (편리해지긴 하지만)
+        // 자바에서는 리모콘의 버튼이 제거되는 것은 늘 안전하다고 판단된다. (불편해지긴 하지만)
     }
 }
 
-class 표준Tv {
-    void 켜기() {
+class 무기 {
 
-    }
-
-    void 끄기() {
-
-    }
 }
 
-class 샤오미Tv extends 표준Tv {
-    void 켜기() {
-        System.out.println("샤오미Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("샤오미Tv 꺼집니다.");
-    }
-
-    void vr켜기() {
-        System.out.println("샤오미Tv vr켜기!");
-    }
-}
-
-class 삼성Tv extends 표준Tv {
-    void 켜기() {
-        System.out.println("삼성Tv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("삼성Tv 꺼집니다.");
-    }
-
-    void ar켜기() {
-        System.out.println("삼성Tv ar켜기!");
-    }
-}
-
-class LGTv extends 표준Tv {
-    void 켜기() {
-        System.out.println("LGTv 켜집니다.");
-    }
-
-    void 끄기() {
-        System.out.println("LGTv 꺼집니다.");
-    }
-
-    void 게임모드전환() {
-        System.out.println("LGTv 게임모드전환!");
-    }
-
+class 칼 extends 무기 {
+    void 공격() { }
 }
