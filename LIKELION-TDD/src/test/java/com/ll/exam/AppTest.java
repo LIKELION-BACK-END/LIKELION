08504ff5e751de9ce9093880ab007d767858707c
zzerowin <alaykr123@gmail.com>
2022-07-05 13:24:21 +0900

    LIKELION-TDD

---
+++ LIKELION-TDD/src/test/java/com/ll/exam/AppTest.java
@@ -0,0 +1,14 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void plus() {
+        int res = 5 + 5;
+        assertEquals(10, res);
+    }
+}
