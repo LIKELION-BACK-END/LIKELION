dc35f79e1191cb1a3f00b786981dbf1539a26416
zzerowin <alaykr123@gmail.com>
2022-07-11 13:07:24 +0900

    문제 - 기본형 변수의 경우 자동형변환을 이용하여 저장 메서드의 개수를 최대한 줄여보세요.(개념 - 자동형변환, 오버로딩)(5개 이하로 줄이세요.)

---
+++ 0711/src/main/java/저장소.java
@@ -0,0 +1,28 @@
+public class 저장소 {
+
+    static void 저장(double v) {
+        System.out.println("double 값 : " + v);
+    }
+
+    static void 저장(boolean v) {
+        System.out.println("boolean 값 : " + v);
+    }
+
+    static void 저장(String v) {
+        System.out.println("String 값 : " + v);
+    }
+
+    static void 저장(사람 v) {
+        System.out.println("사람 값 : " + v);
+    }
+
+    static void 저장(사과 v) {
+        System.out.println("사과 값 : " + v);
+    }
+}
+
+class 사람 {
+}
+
+class 사과 {
+}
