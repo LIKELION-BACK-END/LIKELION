3e79a1caafd06040b61155b139648659cc87952a
zzerowin <alaykr123@gmail.com>
2022-07-14 10:32:05 +0900

    문제 - ArrayList 만들기 문제 3(개념 - 배열의 길이가 필요할 때 자동으로 증가, 사실은 교체)

--- 0714/src/main/java/Main.java
+++ 0714/src/main/java/Main.java
@@ -1,4 +1,6 @@
-// 문제 : 배열의 단점을 보완한 ArrayList 라는 클래스를 만들어주세요. 아래와 같이 출력되도록 해주세요.
+//code.oa.gg/java8/993
+
+// 문제 : 배열의 단점을 보완한 ArrayList 라는 클래스를 만들어주세요. 아래와 같이 출력되도록 해주세요. add 함수에서 배열의 크기가 자동으로 늘어나도록 해주세요.
 
 class Main {
     public static void main(String[] args) {
@@ -6,6 +8,7 @@ class Main {
         ar.add(100);
         ar.add(200);
         ar.add(300);
+        ar.add(400);
 
         int value = ar.get(0);
         System.out.println(value);
@@ -18,21 +21,41 @@ class Main {
         value = ar.get(2);
         System.out.println(value);
         // 출력 : 300
+
+        value = ar.get(3);
+        System.out.println(value);
+        // 출력 : 400
     }
 }
 
 class ArrayList {
     int[] datas;
-    int lastIndex;
+    int lastIndex = -1;
 
     ArrayList() {
-        datas = new int[3];
-        lastIndex = -1;
+        datas = new int[3]; // 이 부분은 수정할 수 없습니다.
     }
 
-    void add(int value) {
+    void add(int data) {
         lastIndex++;
-        datas[lastIndex] = value;
+
+        if (lastIndex + 1 > datas.length) {
+            // 확장공사
+            // 기존버스 버리고 ㅅ 버스로 연결!!
+            // datas 이 녀석이 기존 버스를 버리고 새 버스를 가리켜야 합니다.
+
+            // 새 버스 생성
+            int[] newArr = new int[datas.length * 2];
+
+            // 기존 버스(배열)를 버리기 전에 버스에 있던 승객들을 새 버스로 옮긴다.
+            for (int i = 0; i < datas.length; i++) {
+                newArr[i] = datas[i];
+            }
+
+            datas = newArr;
+        }
+
+        datas[lastIndex] = data;
     }
 
     int get(int index) {
