a64bf2dc6f8933dd9bddf78fe0da208cf1e1e0f5
zzerowin <alaykr123@gmail.com>
2022-07-15 11:17:38 +0900

    ArrayList 클래스 생성 부터, size 함수까지 구현

--- 0714/src/main/java/ArrayList.java
+++ 0714/src/main/java/ArrayList.java
@@ -1,63 +1,65 @@
-class ArrayList {
-    private Object[] datas;
+public class ArrayList {
+
     private int size;
+    private int[] datum;
 
-    ArrayList() {
-        datas = new Object[2];
+    public ArrayList() {
         size = 0;
+        datum = new int[2];
     }
 
-    Integer get(int index) {
-        return (Integer) datas[index];
-    }
-
-    void removeAt(int index) {
-        for (int i = index + 1; i < size; i++) {
-            datas[i - 1] = datas[i];
-        }
-        size--;
-    }
-
-    private void extendToDataSizeIfNeedToAdd() {
-        boolean needToExtend = size >= datas.length;
-
-        if (needToExtend) {
-            Object[] newDatas = new Object[datas.length * 2];
-
-            for (int i = 0; i < datas.length; i++) {
-                newDatas[i] = datas[i];
-            }
-
-            System.out.println("배열의 크기가 증가되었습니다. " + datas.length + " => " + newDatas);
-
-            datas = newDatas;
-        }
-    }
-
-    void add(int data) {
-        add(data, size);
-    }
-
-    void add(int data, int indexToAdd) {
-        extendToDataSizeIfNeedToAdd();
-
-        for (int i = size; i > indexToAdd; i--) {
-            datas[i] = datas[i - 1];
-        }
-
-        datas[indexToAdd] = data;
-        size++;
-    }
-
-    int size() {
+    public int size() {
         return size;
     }
 
-    void showAllValues() {
-        System.out.println("-- 전체 데이터 출력 --");
-        for (int i = 0; i < size; i++) {
-            System.out.print(i + " : ");
-            System.out.println(datas[i]);
+    public void add(int data) {
+        sizeUpIfFull();
+
+        datum[size] = data;
+        size++;
+    }
+
+    private void sizeUpIfFull() {
+        if ( isFull() ) {
+            sizeUp();
+        }
+    }
+
+    private void sizeUp() {
+        int[] newDatum = new int[datum.length * 2];
+
+        for ( int i = 0; i < datum.length; i++ ) {
+            newDatum[i] = datum[i];
+        }
+
+        datum = newDatum;
+    }
+
+    private boolean isFull() {
+        return size == datum.length;
+    }
+
+    public int get(int index) {
+        return datum[index];
+    }
+
+    public void removeAt(int index) {
+        for ( int i = index + 1; i < size; i++ ) {
+            datum[i - 1] = datum[i];
+        }
+
+        size--;
+    }
+
+    public int getArrayLength() {
+        return datum.length;
+    }
+
+    public void showAllValues() {
+        System.out.println("== 전체 데이터 출력 ==");
+
+        for ( int i = 0; i < size; i++ ) {
+            System.out.printf("%d : %d\n", i, datum[i]);
         }
     }
 }
