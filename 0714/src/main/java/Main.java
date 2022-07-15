cb9c47fc40a2572234703d5ac80175f832d8c8c9
zzerowin <alaykr123@gmail.com>
2022-07-15 09:23:09 +0900

    문제 - ArrayList 최종

--- 0714/src/main/java/Main.java
+++ 0714/src/main/java/Main.java
@@ -1,86 +1,162 @@
-//code.oa.gg/java8/993
-
-// 문제 : 배열의 단점을 보완한 ArrayList 라는 클래스를 만들어주세요. 아래와 같이 출력되도록 해주세요. add 함수에서 배열의 크기가 자동으로 늘어나도록 해주세요.
+// 문제 : 아래가 실행되도록 해주세요.
+// 조건 : ArrayList 객체에서 내부적으로 Object 배열을 사용해서 데이터를 저장해야 합니다.
+// 조건 : Object 배열의 초기 길이는 2 입니다.
+// 조건 : 상황에 따라 배열의 길이는 자동으로 증가해야 합니다.
+// 조건 : 엘리먼트(구성요소)를 하나 삭제하면 해당 요소 뒤의 요소들이 전부 앞으로 한칸씩 움직여야 합니다.
+// 조건 : ArrayList의 인스턴스 변수는 2개만 사용할 수 있습니다.
+// 조건 : 외부에서 호출하지 않는 속성은 private 키워드를 붙여주세요.
+// 조건 : 중복을 최대한 제거해주세요.
+// 조건 : 사용하는 변수와 if문, for문의 개수를 가독성을 떨어뜨리지 않는 선에서 최대한 줄여주세요.
 
 class Main {
     public static void main(String[] args) {
-        ArrayList ar = new ArrayList();
-        ar.add(100);
-        ar.add(200);
-        ar.add(300);
-        ar.add(400);
-        ar.add(500);
-        ar.add(600);
-        ar.add(700);
-        ar.add(800);
-        ar.add(900);
-        ar.add(1000);
-        ar.add(1100);
-        ar.add(1200);
-        ar.add(1300);
-        int value = ar.get(0);
-        System.out.println(value);
-        // 출력 : 100
+        ArrayList al = new ArrayList();
 
-        value = ar.get(1);
-        System.out.println(value);
-        // 출력 : 200
+        System.out.println("al.size() : " + al.size());
+        // 출력 => al.size() : 0
 
-        value = ar.get(2);
-        System.out.println(value);
-        // 출력 : 300
+        al.add(100);
 
-        value = ar.get(3);
-        System.out.println(value);
-        // 출력 : 400
+        System.out.println("al.get(0) : " + al.get(0));
+        // 출력 => al.get(0) : 100
+
+        al.add(200);
+        al.add(300);
+        // 출력 => 배열의 크기가 증가되었습니다. 2 => 4
+
+        System.out.println("al.size() : " + al.size());
+        // 출력 => al.size() : 3
+
+        System.out.println("al.get(1) : " + al.get(1));
+        // 출력 => al.get(1) : 200
+
+        al.removeAt(1);
+
+        System.out.println("al.size() : " + al.size());
+        // 출력 => al.size() : 2
+
+        System.out.println("al.get(1) : " + al.get(1));
+        // 출력 => al.get(1) : 300
+
+        al.add(400);
+        al.add(500);
+        al.add(600);
+        // 출력 => 배열의 크기가 증가되었습니다. 4 => 8
+
+        System.out.println("al.get(3) + al.get(4) : " + (al.get(3) + al.get(4)));
+        // 출력 => al.get(3) + al.get(4) : 1100
+
+        System.out.println("al.get(3).intValue() + al.get(4).intValue() : " + (al.get(3).intValue() + al.get(4).intValue()));
+        // 출력 => al.get(3) + al.get(4) : 1100
+
+        al.showAllValues();
+        // 출력 =>
+		/*
+		== 전체 데이터 출력 ==
+		0 : 100
+		1 : 300
+		2 : 400
+		3 : 500
+		4 : 600
+		*/
+
+        al.add(700, 1);
+        al.add(750, 1);
+
+        al.showAllValues();
+        // 출력 =>
+		/*
+		== 전체 데이터 출력 ==
+		0 : 100
+		1 : 750
+		2 : 700
+		3 : 300
+		4 : 400
+		5 : 500
+		6 : 600
+		*/
     }
 }
 
 class ArrayList {
-    int[] datas;
-    int lastIndex = -1;
+    private Object[] datas;
+    private int size;
 
     ArrayList() {
-        datas = new int[3]; // 이 부분은 수정할 수 없습니다.
+        datas = new Object[2];
+        size = 0;
+    }
+
+    Integer get(int index) {
+        return (Integer) datas[index];
+    }
+
+    void removeAt(int index) {
+        for (int i = index + 1; i < size; i++) {
+            datas[i - 1] = datas[i];
+        }
+        size--;
+    }
+
+    private void extendToDataSizeIfNeedToAdd() {
+        boolean needToExtend = size >= datas.length;
+
+        if (needToExtend) {
+            Object[] newDatas = new Object[datas.length * 2];
+
+            for (int i = 0; i < datas.length; i++) {
+                newDatas[i] = datas[i];
+            }
+
+            System.out.println("배열의 크기가 증가되었습니다. " + datas.length + " => " + newDatas);
+
+            datas = newDatas;
+        }
     }
 
     void add(int data) {
-        sizeUpIfFull();
-
-        System.out.printf("데이터 %d 삽입됨\n", data);
-
-        lastIndex++;
-
-        datas[lastIndex] = data;
+        add(data, size);
     }
 
-    void sizeUpIfFull() {
-        if ( isFull() ) {
-            sizeUp();
-        }
-    }
+    void add(int data, int indexToAdd) {
+        extendToDataSizeIfNeedToAdd();
 
-    void sizeUp() {
-        // 1 : 2배 더 큰 배열객체를 만든다.
-        // 2 : 이사한다.
-        // 3 : datas 가 더 이상 기존 배열객체를 가리키지 않고 이번에 새로 만든 배열 객체를 가리킨다.
-
-        int[] newDatas = new int[datas.length * 2];
-
-        for ( int i = 0; i < datas.length; i++ ) {
-            newDatas[i] = datas[i];
+        for (int i = size; i > indexToAdd; i--) {
+            datas[i] = datas[i - 1];
         }
 
-        System.out.printf("사이즈 업(%d => %d)\n", datas.length, newDatas.length);
-
-        datas = newDatas;
+        datas[indexToAdd] = data;
+        size++;
     }
 
-    boolean isFull() {
-        return datas.length - 1 == lastIndex;
+    int size() {
+        return size;
     }
 
-    int get(int index) {
-        return datas[index];
+    void showAllValues() {
+        System.out.println("-- 전체 데이터 출력 --");
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + " : ");
+            System.out.println(datas[i]);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
