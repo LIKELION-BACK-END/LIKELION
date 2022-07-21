2671f71e41e31aeffda407a43fc580484b324545
zzerowin <alaykr123@gmail.com>
2022-07-21 10:20:51 +0900

    문제 - 다른 객체가 해당 메서드를 이용하게 해주세요. 인터페이스 사용

--- 0721/src/main/java/Main.java
+++ 0721/src/main/java/Main.java
@@ -1,33 +1,42 @@
 /*
-문제 : Main 객체의 안녕이 Main클래스가 아닌 다른 클래스 객체에 의해 실행되도록 해주세요.
+문제 : 사람 객체의 `안녕`이 `button.click();`에 의해 실행되도록 해주세요.
+조건 : Button에는 `사람` 이라는 언급이 없어야 합니다.
 */
 class Main {
     public static void main(String[] args) {
-
-        Main aMain = new Main();
-
-        실행자 a실행자 = new 실행자();
-        a실행자.set리모콘(aMain);
-        a실행자.리모콘_작동();
-    }
-
-    public void 안녕() {
-        System.out.println("Main::안녕!");
+        Button button = new Button();
+        button.setListener(new 사람());
+        button.setListener(new 코끼리());
+        button.click();
     }
 }
 
-class 실행자 {
-    private Object a리모콘;
+interface Listener {
+    public void listen(String msg);
+}
 
-    public void set리모콘(Object a리모콘) {
-        this.a리모콘 = a리모콘;
+class Button {
+    private Listener aListener;
+    public void setListener(Listener aListener) {
+        this.aListener = aListener;
     }
+    public void click() {
+        aListener.listen("버튼 클릭 되었네요.");
+    }
+}
 
-    public void 리모콘_작동() {
-        // 수정가능지역 시작
-        if (a리모콘 instanceof Main) {
-            ((Main)a리모콘).안녕();
-        }
-        // 수정가능지역 끝
+class 사람 implements Listener {
+    @Override
+    public void listen(String msg) {
+        안녕();
+    }
+    public void 안녕() {
+        System.out.println("사람, 안녕!");
+    }
+}
+
+class 코끼리 implements Listener {
+    public void listen(String msg) {
+        System.out.println("꼬끼리가 버튼이 클릭되었다는 소식을 받았습니다,\n메세지 : " + msg);
     }
 }
