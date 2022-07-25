24d91baab1fc0169593850bb0750874bb2328ee3
zzerowin <alaykr123@gmail.com>
2022-07-25 10:45:44 +0900

    문제 - 익명내부클래스와 인터페이스를 사용해서 문제를 풀어주세요. (람다)

--- 0725/src/main/java/Main.java
+++ 0725/src/main/java/Main.java
@@ -2,16 +2,9 @@ class Main {
     public static void main(String[] args) {
         View.OnClickListener aOnClickListener;
 
-        // 수정가능지역 시작
-        class Listener implements View.OnClickListener {
-
-            @Override
-            public void onClick() {
-                System.out.println("클릭되었다는 사실을 전달받았습니다.");
-            }
-        }
-
-        aOnClickListener = new Listener();
+        aOnClickListener = () -> {
+            System.out.println("ss");
+        };
         // 수정가능지역 끝
 
         aOnClickListener.onClick();
